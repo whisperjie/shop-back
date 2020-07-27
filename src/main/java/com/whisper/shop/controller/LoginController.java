23633d0c235198f6b218938ab3c907bdce63075e
whisper <1416207703@qq.com>
2020-07-27 13:39:32 +0800

    add controller and entity

---
+++ src/main/java/com/whisper/shop/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.whisper.shop.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class LoginController {
+
+}
